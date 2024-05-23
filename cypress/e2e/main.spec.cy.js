b620a443b87f4f9af617c28f74c30d531f59fe68
Jardele <jadeleferreira@gmail>
2024-05-23 11:46:43 -0300

    First cenario cypress autimation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -3,9 +3,9 @@ describe('template spec', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
-    cy.get('[data-qa="publish-button"]').first().click()
+    cy.get('[data-qa="publish-button"]')me.first().click()
     cy.get('[data-qa="comment-text"]').contains('Test 123')
-    cy.get('[data-qa="comment-text"]').each(($element) => {
+    cy.get('[data-qa="comment-text"]').each(($elent) => {
       if ($element.text() === 'Test 123') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
